29b6c8b1380c013d9f416f6f7148e3f010bc55dd
harmanlaiba-blip <harmanlaiba@gmail.com>
2026-06-29 11:20:58 +0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,19 @@
+#Day 3
+name = input("Enter your Name:")
+print("Hello,", name)
+
+name = input("Enter your name:")
+age = input("Enter your age:")
+city=input("Enter your city:")
+
+print("\n ---User Profil3---")
+print("Name", name)
+print("Age", age)
+print("City", city)
+
+print("\n type of age after conversion:", type(age))
+
+#Task
+num1 = int(input("Enter 1st Number:"))
+num2 = float(input("Enter 2nd Number:"))
+print(num1+num2)
